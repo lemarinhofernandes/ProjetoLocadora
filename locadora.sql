1404cd3fa5940cb5d5e82b194b7196204bc2255a
Luis Eduardo Marinho Fernandes <duduyugi@gmail.com>
2021-10-18 08:43:20 -0300

    adiciona exemplo

--- locadora.sql
+++ locadora.sql
@@ -15,24 +15,20 @@ CREATE TABLE IF NOT EXISTS cliente (
   PRIMARY KEY (idCliente)
   );
 
-
 CREATE TABLE IF NOT EXISTS telefone (
   idtelefone INT NOT NULL,
   numero VARCHAR(11) NOT NULL,
   idCliente INT NOT NULL,
-  telefonecol VARCHAR(45) NULL,
   PRIMARY KEY (idtelefone),
   FOREIGN KEY (idCliente)
     REFERENCES cliente (idCliente)
 );
-
 
 CREATE TABLE IF NOT EXISTS funcionarios (
   idfuncionario INT NOT NULL,
   nomeFun VARCHAR(100) NOT NULL,
   PRIMARY KEY (idfuncionario)
   );
-
 
 CREATE TABLE IF NOT EXISTS atende (
   idCliente INT NOT NULL,
@@ -43,7 +39,6 @@ CREATE TABLE IF NOT EXISTS atende (
     REFERENCES funcionarios (idfuncionario)
 );
 
-
 CREATE TABLE IF NOT EXISTS pagamento (
   tipoPagamento ENUM('PIX', 'DEB', 'CRED') NOT NULL,
   idCliente INT NOT NULL,
@@ -51,40 +46,35 @@ CREATE TABLE IF NOT EXISTS pagamento (
   FOREIGN KEY (idCliente)
     REFERENCES cliente (idCliente),
   FOREIGN KEY (idFuncionario)
-    REFERENCES funcionario (idfuncionario)
+    REFERENCES funcionarios (idfuncionario)
 );
-
 
 CREATE TABLE IF NOT EXISTS filmes (
   idfilme INT NOT NULL,
   nomeFilme VARCHAR(100) NULL,
-  classificacaoFilme INT NULL,
+  classificacaoFilme ENUM("L, 10, 12, 14, 16 ,18") NULL,
   lancamentoFilme DATE NULL,
   PRIMARY KEY (idfilme)
 );
 
+  CREATE TABLE IF NOT EXISTS distribuidoras (
+  nomeDistri VARCHAR(100) NOT NULL,
+  cnpjDistri VARCHAR(14) NULL,
+  enderecoDistri VARCHAR(100) NULL,
+  idfilme INT NOT NULL,
+  FOREIGN KEY (idFilme)
+    REFERENCES filmes (idfilme)
+);
 
 CREATE TABLE IF NOT EXISTS emprestimo (
   idCliente INT NOT NULL,
   dataPedido DATE NULL,
   idFilme INT NOT NULL,
-  emprestimocol VARCHAR(45) NULL,
   FOREIGN KEY (idFilme)
     REFERENCES filmes (idfilme),
   FOREIGN KEY (idCliente)
     REFERENCES cliente (idCliente)
 );
-
-
-CREATE TABLE IF NOT EXISTS distribuidoras (
-  nomeDistri VARCHAR(100) NOT NULL,
-  cnpjDistri VARCHAR(14) NULL,
-  enderecoDistri VARCHAR(100) NULL,
-  filmes_idfilme INT NOT NULL,
-    FOREIGN KEY (filmes_idfilme)
-    REFERENCES filmes (idfilme)
-);
-
 
 CREATE TABLE IF NOT EXISTS categoria (
   idFilme INT NOT NULL,
@@ -98,7 +88,6 @@ CREATE TABLE IF NOT EXISTS categoria (
     REFERENCES filmes (idfilme)
 );
 
-
 CREATE TABLE IF NOT EXISTS atores (
   idFilme INT NOT NULL,
   nomeAtor VARCHAR(100) NULL,
@@ -106,14 +95,12 @@ CREATE TABLE IF NOT EXISTS atores (
     REFERENCES filmes (idfilme)
 );
 
-
 CREATE TABLE IF NOT EXISTS diretor (
   idFilme INT NOT NULL,
   nomeDiretor VARCHAR(100) NULL,
   FOREIGN KEY (idFilme)
     REFERENCES filmes (idfilme)
 );
-
 
 CREATE TABLE IF NOT EXISTS traducao (
   idtraducao INT NOT NULL,
@@ -123,3 +110,36 @@ CREATE TABLE IF NOT EXISTS traducao (
   FOREIGN KEY (idFilme)
     REFERENCES filmes (idfilme)
 );
+
+INSERT INTO cliente VALUES
+(1, "Luís Eduardo", "11111111111", "email@example.com", 22, "Samambaia Sul");
+
+INSERT INTO telefone VALUES
+(1, "61985456156", 1);
+
+INSERT INTO funcionarios VALUES
+(1, "Fábio");
+
+INSERT INTO atende VALUES
+(1, 1);
+
+INSERT INTO pagamento VALUES
+("PIX", 1, 1);
+
+INSERT INTO filmes VALUES
+(1, "The Mummy", "12", '18/06/1999');
+
+INSERT INTO distribuidoras VALUES
+("distribuidora de filme", "11111111111111", "Rua das Laranjeiras, Casa 20", 1);
+
+INSERT INTO emprestimo VALUES
+(1, '18-10-2021', 1);
+
+INSERT INTO categoria VALUES
+(1, 0, 0, 1, 0, 0, 0);
+
+INSERT INTO atores VALUES
+(1,"Brendan Fraser");
+
+INSERT INTO traducao VALUES
+(1, "A Mumia", 1);
